22dbbf5e16732d0d2660fbe48d3c2753cc105dec
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-02-25 13:43:27 +0700

    Update .gitignore and enhance grading logic in Minitask-2.js

--- Minitask-2.js
+++ Minitask-2.js
@@ -12,6 +12,23 @@ if (score <= 100 && score >= 90) {
   console.log("Grade F");
 }
 
+switch (true) {
+  case score >= 90 && score <= 100:
+    console.log("Grade A");
+    break;
+  case score >= 80 && score < 90:
+    console.log("Grade B");
+    break;
+  case score >= 70 && score < 80:
+    console.log("Grade C");
+    break;
+  case score >= 60 && score < 70:
+    console.log("Grade D");
+    break;
+  default:
+    console.log("Grade F");
+}
+
 const gender = "L"
 
 switch (gender) {
