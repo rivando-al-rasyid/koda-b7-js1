f98c77a074692961cf40b6e5d282d2cdf3270dee
Rivando Al rasyid <119521535+rivando-al-rasyid@users.noreply.github.com>
2026-02-25 12:56:12 +0700

    Add files via upload

---
+++ Minitask-1.js
@@ -0,0 +1,14 @@
+
+let who = {am : {i : "rivando al Rasyid"} }
+console.log(who.am.i)
+
+
+let Hello = {World : "Hello World"} 
+console.log(Hello.World)
+
+let obj = {str : [1,[1,"fazztrack",[1,"lets","go" ,{koda : [{tech :  {academy:"Koda Tech Academy"}}]}]]] }
+console.log(obj.str[1][2][3].koda[0].tech["academy"])
+
+
+let my = [{very : {best : [1,{sport : {is : "Football"}}]}}]
+console.log(my[0].very["best"][1].sport.is)
